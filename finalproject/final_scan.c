be4d33f3def1ae8c4b03746550830f206637622e
Tran <kevtran04@gmail.com>
2025-05-01 21:22:10 -0500

    started working on a idle scan, very uncomplete

--- finalproject/final_scan.c
+++ finalproject/final_scan.c
@@ -5,6 +5,7 @@
 #include "Timer.h"
 #include <math.h>
 #include "open_interface.h"
+#include "final_uart.h"
 
 #define TAPE_THRESHOLD 2700
 #define CLIFF_THRESHOLD 500
@@ -27,9 +28,12 @@ void scan(int angle, cyBot_Scan* getScan) {
     ping_trigger();
     timer_waitMillis(2);//take a measurement every 500 ms
     if(update_flag >= 2) {
-        uart_sendStr("RAH");
+
         time_diff = last_time - current_time;
-        getScan->sound_dist = ping_getDistance(time_diff, &ms);  // PING sensor
+
+        float distance1 = ping_getDistance(time_diff, &ms);
+        float distance2 = ping_getDistance(time_diff, &ms);
+        getScan->sound_dist = (distance1 + distance2) / 2;  // PING sensor
         timer_waitMillis(500);
         update_flag = 0;
     }
@@ -45,7 +49,6 @@ void scanFullForObjects(void) {
 
     int angle;
     for (angle = 0; angle <= 180; angle += 2) {
-        uart_sendStr("yo");
         scan(angle, &scanner);
         double ir_cm = 157000.0 * pow(scanner.IR_raw_val, -1.176);
 
@@ -80,9 +83,53 @@ bool checkBumpOrCliffDuringMove(oi_t* sensor_data) {
  * This function is used to scan while moving.
  */
 void idleScan(void) {
-    uart_sendStr("This is idle scan");
+    int start_angle = 45;
+    int end_angle = 135;
+    int step = 5;
+    char buffer[90];
+    cyBot_Scan scanner;
+    int i = 0;
+    float distances[19]; //  (Up to (135-45) / 5 + 1 = 19)
+    int angles[19];
+    int min_angle = 90;
+    int angle;
+    uart_sendStr("This is idle scan\r\n");
 
+    /*
+     * SCAN FOR DATA
+     */
+    for(angle = start_angle; angle <= end_angle; angle+=step, i++) {
+        scan(angle, &scanner);
+        distances[i] = scanner.sound_dist;
+        angles[i] = angle;
+        sprintf(buffer, "Angle:%3d, Distance %.2f cm\r\n", angle, scanner.sound_dist);
+        uart_sendStr(buffer);
+    }
+
+    /*
+     * FIND THE BIGGEST GAP
+     */
+
+    float largest_gap = 0.0f;
+    int gap_index = 0;
+    int j = 0;
+    int num_points = (end_angle - start_angle) / step + 1;
+
+    for(j = 0; j < num_points - 1; j++) {
+        float gap = distances[j + 1] - distances[j];
+        if (gap > largest_gap) {
+            largest_gap = gap;
+            gap_index = j;
+        }
+    }
+
+    int gap_angle = (angles[gap_index] + angles[gap_index + 1]) / 2;
+
+    servo_move(gap_angle);
+    sprintf(buffer, "turning to midpoint angle: %d\r\n", gap_angle);
+    uart_sendStr(buffer);
 
 }
+
 
 
